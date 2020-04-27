2e982d414d4fcb3ed744ee6dcbcb233c4b837749
MichielCasteleyn <michiel.casteleyn@outlook.com>
2020-04-27 21:37:00 +0200

    caching

--- src/functions/show/show.js
+++ src/functions/show/show.js
@@ -7,19 +7,29 @@ const client = contentful.createClient({
   accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN,
 })
 
-const fetch = require('node-fetch')
+// const fetch = require('node-fetch')
+const storage = new Map()
+
 exports.handler = async function(event, context) {
   try {
     const id = event.queryStringParameters.id.replace("/", "")
+    let vuistje;
 
-    const space = await client.getSpace(process.env.CONTENTFUL_SPACE_ID)
-    const environment = await space.getEnvironment("master")
-    const entry = await environment.getEntry(id)
+    if (storage.has(id)) {
+      console.log("In storage gevonden")
+      vuistje = storage.get(id)
+    } else {
+      const space = await client.getSpace(process.env.CONTENTFUL_SPACE_ID)
+      const environment = await space.getEnvironment("master")
+      const entry = await environment.getEntry(id)
 
-    vuistje = {
-      to: entry.fields.to["en-US"],
-      from: entry.fields.from["en-US"],
-      message: entry.fields.message["en-US"],
+      vuistje = {
+        to: entry.fields.to["en-US"],
+        from: entry.fields.from["en-US"],
+        message: entry.fields.message["en-US"],
+      }
+      console.log("In storage plaatsen")
+      storage.set(id, vuistje)
     }
     // console.log("In storage plaatsen")
     // storage.set(id, vuistje)
